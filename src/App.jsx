2582be2f6e15b05cef868dd6568ecf3039eebffd
Christopher Acosta <chrisacosta12345@gmail.com>
2023-12-15 17:09:37 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { withAuth0 } from '@auth0/auth0-react';
+
+import AuthButtons from './Auth/Buttons.jsx';
+
+import Dogs from './components/Dogs.jsx';
+
+function App(props) {
+    console.log(props);
+    return(
+      <>
+        <h1>Our App</h1>
+        <AuthButtons />
+        <hr />
+        {
+          props.auth0.isAuthenticated &&
+            <>
+              <Dogs />
+            </>
+        }
+      </>
+    )
+}
+
+
+// export default App;
+export default withAuth0(App);
+
---
+++ src/components/Dogs.jsx
@@ -0,0 +1,41 @@
+import {useState, useEffect} from 'react';
+import { withAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+
+function Dogs(props) {
+
+  const [dogs, setDogs] = useState([]);
+
+  useEffect( () => {
+
+    async function getDogs() {
+
+      let claim = await props.auth0.getIdTokenClaims();
+      let token = claim.__raw;
+
+      const config = {
+        headers: { "Authorization": `Bearer ${token}` },
+        method: "get",
+        url: 'http://localhost:3000/dogs',
+      }
+
+      let resposne = await axios(config);
+      let data = resposne.data;
+      setDogs(data);
+    }
+
+    getDogs();
+
+  }, []);
+
+  return (
+    <ul>
+      {
+        dogs.map( book => <li key={book._id}>{book.title}</li> )
+      }
+    </ul>
+  )
+
+}
+
+export default withAuth0(Dogs);
